eeade8a88eacc0b661e749fee93f0cc5a6e84abb
MEMEO-PRO <yash.ecs@gmail.com>
2023-05-18 02:40:28 +0530

    files added

--- Main.java
+++ Main.java
@@ -1,32 +1,27 @@
 import java.util.Scanner;
 import java.util.Vector;
 
-public class Main{
-    public static void main(String[] args){
-        Vector<String> stringVector = new Vector<>();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of strings to add: ");
-        int n = sc.nextInt();
-        for(int i = 0; i<n; i++){
-            String str = sc.nextLine();
-            stringVector.add(str);
-
+public class Main {
+    public static void main(String[] args) {
+        Vector<String> studentNames = new Vector<>();
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter student names (type 'exit' to stop):");
+        
+        String name;
+        do {
+            name = scanner.nextLine();
+            if (!name.equalsIgnoreCase("exit")) {
+                studentNames.add(name);
+            }
+        } while (!name.equalsIgnoreCase("exit"));
+        
+        System.out.println("Student names entered:");
+        for (String studentName : studentNames) {
+            System.out.println(studentName);
         }
-        System.out.print("Enter a new string to check: ");
-        String newString = sc.nextLine();
-
-        if(stringVector.contains(newString)){
-            stringVector.remove(newString);
-            System.out.println("deleted");
-
-        }
-
-        System.out.println("Updated vector: ");
-        for(String str : stringVector){
-            System.out.println(str);
-        }
-        sc.close();
+        
+        scanner.close();
     }
-
-    
 }
